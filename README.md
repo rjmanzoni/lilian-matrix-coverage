e862e25245856f40278ca23b9a582191fb215ade
Renato de Jesus Manzoni <rmanzoni@localhost.localdomain>
2018-03-06 18:48:09 -0300

    ajustes

--- README.md
+++ README.md
@@ -6,4 +6,3 @@
 
 ## How to Run
 * `sh generate-report-test.sh`
-* `codecover-batch-1.0/codecover.sh analyze --container icmc_tsc.xml --coverage-log coverage-log-<date>.clf --name JUnitTestSession --comment "Two errors occurred."`
--- generate-report-test.sh
+++ generate-report-test.sh
@@ -2,6 +2,9 @@
 
 BASEDIR=$(dirname "$0")
 
+rm -f $BASEDIR/coverage-log*
+rm -f icmc_tsc.xml
+
 sh $BASEDIR/codecover-batch-1.0/codecover.sh instrument --root-directory $BASEDIR/src/main/java/ --destination src.instr --container icmc_tsc.xml --language java --verbose
 
 mvn clean test
@@ -10,5 +13,5 @@ find src.instr/ -name *.java | xargs javac
 
 java -cp libs/junit-4.2.jar:codecover-batch-1.0/lib/JUnit-TestRunner.jar:codecover-batch-1.0/lib/plugins/org.codecover.instrumentation.java15-0.1.jar:src.instr/:target/test-classes/ org.codecover.junit4.core.TestRunner -useMethodsAsTestcases usp.icmc.ATest usp.icmc.BTest
 
-# ESSA parte aqui rode a parte trocando o log coverage-log-2018-03-06-16-54-36-070.clf pelo que foi gerado
-#codecover-batch-1.0/codecover.sh analyze --container icmc_tsc.xml --coverage-log coverage-log-2018-03-06-16-54-36-070.clf --name JUnitTestSession --comment "Two errors occurred."
+file=$(find $BASEDIR -name "coverage*")
+sh  $BASEDIR/codecover-batch-1.0/codecover.sh analyze --container icmc_tsc.xml --coverage-log $file --name JUnitTestSession
